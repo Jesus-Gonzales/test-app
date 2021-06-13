b6788431ebae4e2508b94ddb3d5feed2a9919542
Jesus-Gonzales <jetus1999@gmail.com>
2021-06-13 19:33:46 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import App2 from './App2';
+
+export default function App() {
+  return (
+    <div className="App">
+      <App2></App2>
+      <header className="App-header">
+        <h2>Mi nueva página</h2>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App2.js
@@ -0,0 +1,5 @@
+export default function App2() {
+    return {
+        < div > Mi nuevo componente</div >
+    };
+}
